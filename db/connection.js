3368d04c931741ae96a29bf88e406464e1e0e610
Chandan Kanaujia <106033763+chandanegc@users.noreply.github.com>
2024-08-11 21:48:27 +0530

    Update connection.js
    ere

--- db/connection.js
+++ db/connection.js
@@ -2,12 +2,12 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 const main = async ()=>{
-    mongoose.connect(process.env.MONG_URL ,({
+    mongoose.connect("mongodb+srv://Ecommers:Ytp4PMNnVZiQWU82@cluster0.kioi2eo.mongodb.net/?retryWrites=true&w=majority" ,({
         useNewUrlParser:true,
         useUnifiedTopology:true
     }))
 }
 main().then(()=>{
-    console.log("DB connection successfull......" , process.env.PORT)
+    console.log("DB connection successfull......" , 5000)
 })
 .catch((e)=>{});
